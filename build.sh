208a6f41232fcebe19baae8f82b858d95a5de46f
Patryk Miszczak <patryk.miszczak1996@gmail.com>
2022-09-20 20:30:53 +0200

    remove unnecessary character

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 curl -s https://api.github.com/repos/mozilla/mozjpeg/releases/latest \
 | grep "mozjpeg*.zip" \
 | cut -d : -f 2,3 \
 | tr -d \" \
+| tr -d , \
 | wget -qi -
 
 unzip v*.zip
